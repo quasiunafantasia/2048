1ee75bf4b855c69946e1e1af5702604911410b82
Oleksandr <agoldfarb@ukr.net>
2014-08-04 17:08:22 +0300

    comit

--- js/script.js
+++ js/script.js
@@ -2,12 +2,33 @@ var up = 'up',
 	down = 'down',
 	left = 'left',
 	right = 'right';
-
+	
 window.onload = function () {
-	var field = new FieldModule.Field();	
-	field.generate();
-	field.generate();
-	field.print();
+/*
+	var docWidth, docHeight;
+	docWidth = document.body.clientWidth;
+    docHeight = document.body.clientHeight;
+	var size = docWidth < docHeight? docWidth:docHeight;
+	var container = document.getElementsByClassName("game")[0];
+	container.style.width = container.style.height = size * 0.6+"px";
+	var cells = document.getElementsByClassName("cell");
+	for ( var i = 0; i < cells.length; i++ ){
+		cells[i].style.width = cells[i].style.height = size * 0.6 * 0.2+"px";
+		cells[i].style.margin = size * 0.025 * 0.2+"px";		
+	}*/
+	var field;
+	var gen = function(){
+		field = new FieldModule.Field();	
+		field.generate();
+		field.generate();
+		field.print();
+		}();
+	var move = function(direction){
+		field.step(direction);
+			if (field.gameOver())
+				if (confirm("Good job! Your score is " + field.getScore() +"\n Do you want to start a new game?"))
+					gen();
+	};
 	/* setUnselectable = function(node){ //did not work out
 		if (node.nodeType == 1) {
 			node.unselectable = true;
@@ -19,76 +40,67 @@ window.onload = function () {
 		}
 	}
 	setUnselectable(document.body); */
-		//$.mobile.defaultPageTransition = 'none';
-	/* same
-	var lastScrollTop = 200;
-	$(window).scroll(function(event){
-    var st = $(this).scrollTop();
-	var st2 = $(this).scrollDown();
-	if (st > lastScrollTop){
-       // downscroll code
-	   field.ste(up);
-   } else {
-      // upscroll code
-	  field.step(down);
-   }
-   lastScrollTop = st;
-   //alert("st = " +st);
-	//alert("last scroll top " + lastScrollTop);
-	//alert("down " + st2);
-	});*/
-	
-		$( "body" ).on( "swiperight", function(){field.step(right);
-			if (field.gameOver())
-				if (confirm("Good job! Your score is " + field.getScore() +"\n Do you want to start a new game?")){
-					field = new FieldModule.Field();					
-					field.generate();
-					field.generate();
-					field.print();} 
+	/*	$( "body" ).on( "swiperight", function(){
+		move(right);
+		});
+	$( "body" ).on( "swipeleft", function(){
+		move(left);
 	});
-	
-		$( "body" ).on( "swipeleft", function(){field.step(left);
-			if (field.gameOver())
-				if (confirm("Good job! Your score is " + field.getScore() +"\n Do you want to start a new game?")){
-					field = new FieldModule.Field();					
-					field.generate();
-					field.generate();
-					field.print();} 
+	$( "body" ).on( "swipedown", function(event){
+		move(down);
 	});
-	
-		$( "body" ).on( "swipedown", function(){field.step(down);
-			if (field.gameOver())
-				if (confirm("Good job! Your score is " + field.getScore() +"\n Do you want to start a new game?")){
-					field = new FieldModule.Field();					
-					field.generate();
-					field.generate();
-					field.print();}
-	});
-	
-		$( "body" ).on( "swipeup", function(){field.step(up);
-			if (field.gameOver())
-				if (confirm("Good job! Your score is " + field.getScore() +"\n Do you want to start a new game?")){
-					field = new FieldModule.Field();					
-					field.generate();
-					field.generate();
-					field.print();} 
-	});
-	document.onkeydown = function(event){
-	var events = event || window.event;
-		if (events.keyCode == 37) 
-			field.step(left);
-		if (events.keyCode == 39) 
-			field.step(right);
-		if (events.keyCode == 38) 
-			field.step(up);
-		if (events.keyCode == 40) 
-			field.step(down);
-		if (field.gameOver())
-			if (confirm("Good job! Your score is " + field.getScore() +"\n Do you want to start a new game?")){
-				field = new FieldModule.Field();					
-				field.generate();
-				field.generate();
-				field.print();
+	$( "body" ).on( "swipeup", function(event){
+		move(up);
+	});	*/
+	var startX,startY, distX, distY;
+	document.body.addEventListener('touchstart', function(e){
+	var touchobj = e.changedTouches[0]; // reference first touch point (ie: first finger)
+		startX = parseInt(touchobj.clientX);
+		startY = parseInt(touchobj.clientY);
+		e.preventDefault();
+		}, false);
+	/* document.body.addEventListener('touchmove', function(e){
+	  var touchobj = e.changedTouches[0] // reference first touch point for this event
+	   
+	   //alert("x :" + distX+" y: " + distY);
+	   e.preventDefault();
+	 }, false);*/
+	 document.body.addEventListener('touchend', function(e){
+		var touchobj = e.changedTouches[0]; // reference first touch point for this event
+		distX = parseInt(touchobj.clientX) - startX;
+		distY = parseInt(touchobj.clientY) - startY;
+		 if ( Math.abs(distX) >  2 || Math.abs(distY) > 2){
+			if (Math.abs(distX) > Math.abs(distY)){
+				if (distX > 0){
+					move(right);
+				}
+				if (distX < 0){
+					move(left);
+				}
 			}
+			else if (Math.abs(distX) < Math.abs(distY)){
+				if (distY > 0){
+					move(down);
+				}
+				if (distY < 0){
+					move(up);
+				}	
+			}
+		}
+		e.preventDefault()
+		}, false);
+document.onkeydown = function(event){
+var events = event || window.event;
+	if (events.keyCode == 37) 
+		field.step(left);
+	if (events.keyCode == 39) 
+		field.step(right);
+	if (events.keyCode == 38) 
+		field.step(up);
+	if (events.keyCode == 40) 
+		field.step(down);
+	if (field.gameOver())
+		if (confirm("Good job! Your score is " + field.getScore() +"\n Do you want to start a new game?"))
+			gen();
 	}
 }
