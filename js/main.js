45b80956371093339554b7618cd4f5513549f02c
Oleksandr <agoldfarb@ukr.net>
2014-08-05 19:16:01 +0300

    minor issue fixed

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 window.onload = function () {
 	'use strict';
 	var field, field2, gameField, docWidth, docHeight, size;
-	field = new FieldModule.Field(document.getElementById('instance1'),8,8);
+	field = new FieldModule.Field(document.getElementById('instance1'), 8, 8);
 	field.assign(document.getElementById('instance1'));
 	field.generate();
 	field.generate();
@@ -13,12 +13,14 @@ window.onload = function () {
 	size = docWidth < docHeight ? docWidth : docHeight;
 	size = size > 400 ? 400 : size;
 	size *= 0.7;
-	UIModule.adjustGraphics(gameField, size, 0.01, 8);
+	UIModule.adjustGraphics(gameField, size, 0.1, 8);
 	InteractionModule.setUserInteraction(document.getElementById('instance1'), field);
-	field2 = new FieldModule.Field(document.getElementById('instance2'),4,4);
-	gameField = document.getElementById('instance1');
+	field2 = new FieldModule.Field(document.getElementById('instance2'), 4, 4);
+	gameField = document.getElementById('instance2');
 	field2.generate();
 	field2.generate();
 	field2.print();
-	UIModule.adjustGraphics(gameField, size, 0.1, 4);
+	UIModule.adjustGraphics(gameField, size, 0.2, 4);
+	InteractionModule.setUserInteraction(document.getElementById('instance2'), field2);
+	
 };
